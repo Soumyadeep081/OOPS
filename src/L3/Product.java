e09f3d0f25c45f4eb602da9140de60b0030c5b21
Soumyadeep081 <soumyadeep323@gmail.com>
2025-07-16 23:16:24 +0530

    Second Pillar of OOPs

---
+++ src/L3/Product.java
@@ -0,0 +1,21 @@
+package L3;
+
+abstract class Product {
+
+    protected String name;
+
+    protected double price;
+
+    public Product(String name,double price) {
+
+        this.name = name;
+        this.price = price;
+    }
+
+    abstract double calculateDiscount();
+
+    abstract double calculateTax();
+
+    abstract double calculateShippingCost();
+
+}
